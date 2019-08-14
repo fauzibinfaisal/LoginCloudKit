78d9940a8388ce5b7fbb84f1c6f5980bc144b9b9
Fauzi <fauzibinfaisal@gmail.com>
2019-08-14 14:04:08 +0700

    Initial Commit

---
+++ LoginCloudKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginCloudKit
+//
+//  Created by Fauzi Fauzi on 14/08/19.
+//  Copyright © 2019 fauzify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
